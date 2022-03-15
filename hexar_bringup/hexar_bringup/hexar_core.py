fix error calculation
also fix right wheel stuck on right turn bug

hexar_bringup/hexar_bringup/hexar_core.py
@@ -1,5 +1,6 @@
 import serial
 from gpiozero import LED, Motor
+import numpy as np
 import rclpy
 from rclpy.node import Node
 
@@ -9,7 +10,7 @@ from geometry_msgs.msg import Twist
 class HexaRobotCore(Node):
 
     def __init__(self):
-        super().__init__('hexa_subr_pubr')
+        super().__init__('hexar_core')
         # setup serial comm
         self.ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
         self.ser.reset_input_buffer()
@@ -28,15 +29,15 @@ class HexaRobotCore(Node):
             Twist, 
             'cmd_vel', 
             self.cmdv_sub_cb, 
-            10
+            1
         )
         self.cmdv_sub  # prevent unused variable warning
         # setup pid controller
         self.ctrl_timer = self.create_timer(0.02, self.ctrl_cb)
         # constants
-        self.WHEEL_RADIUS = 0.08
+        self.WHEEL_RADIUS = 0.04
         self.WHEEL_SEPARATION = 0.19
-        self.K_P = 0.01
+        self.K_P = .001
         # variables
         self.lin_x = 0.
         self.ang_z = 0.
@@ -56,7 +57,7 @@ class HexaRobotCore(Node):
     def cmdv_sub_cb(self, msg):
         self.targ_lin_x = msg.linear.x
         self.targ_ang_z = msg.angular.z
-        # self.get_logger().info('Target Velocity: "%s"' % msg)
+        self.get_logger().info('Target Velocity: "%s"' % msg)
 
     def vel_pub_cb(self):
         """
@@ -78,19 +79,21 @@ class HexaRobotCore(Node):
         lwhl_vel_lin = self.lwhl_vel * self.WHEEL_RADIUS
         rwhl_vel_lin = self.rwhl_vel * self.WHEEL_RADIUS
         # print(f"lwhl linear, rwhl linear: {lwhl_vel_lin, rwhl_vel_lin}")
-        # 
         msg = Twist()
         self.lin_x = (lwhl_vel_lin + rwhl_vel_lin) * .5
         self.ang_z = (rwhl_vel_lin - lwhl_vel_lin) / self.WHEEL_SEPARATION
         msg.linear.x = self.lin_x
         msg.angular.z = self.ang_z
         self.vel_pub.publish(msg)
-        self.get_logger().info(f"Actual Velocity: {msg}")
+        # self.get_logger().info(f"Actual Velocity: {msg}")
 
     def ctrl_cb(self):
-        self.targ_lwhl_vel = (self.targ_lin_x - (self.targ_ang_z *self.WHEEL_SEPARATION) * .5)
-        self.targ_rwhl_vel = (self.targ_lin_x + (self.targ_ang_z *self.WHEEL_SEPARATION) * .5)
-        # print(f"target wheel speed: {self.targ_lwhl_vel, self.targ_rwhl_vel}")
+        targ_lwhl_vel_lin = (self.targ_lin_x - (self.targ_ang_z *self.WHEEL_SEPARATION) * .5)
+        targ_rwhl_vel_lin = (self.targ_lin_x + (self.targ_ang_z *self.WHEEL_SEPARATION) * .5)
+        self.targ_lwhl_vel = targ_lwhl_vel_lin / self.WHEEL_RADIUS
+        self.targ_rwhl_vel = targ_rwhl_vel_lin / self.WHEEL_RADIUS
+        print(f"target wheel speed: {self.targ_lwhl_vel, self.targ_rwhl_vel}")
+        print(f"actual wheel speed: {self.lwhl_vel, self.rwhl_vel}")
         # set wheel directions based on target wheel velocity
         if self.targ_lwhl_vel > 0:
             self.lwhl_dir = 1
@@ -106,21 +109,21 @@ class HexaRobotCore(Node):
             self.rwhl_dir = 0
         # print(f"wheel direction: {self.lwhl_dir, self.rwhl_dir}")  # debug
         # calculate duty cycle change
-        lerr = abs(self.targ_lwhl_vel) - abs(self.lwhl_vel)
+        lerr = np.abs(self.targ_lwhl_vel) - np.abs(self.lwhl_vel)
         ldcinc = self.K_P * lerr  # duty cycle increment
-        # print(f"left error, left dutycycle increment: {lerr, ldcinc}")
         self.dutycycle_left += ldcinc
         if self.dutycycle_left >= 1.:
             self.dutycycle_left = 1.
         elif self.dutycycle_left <= 0.:
             self.dutycycle_left = 0
-        rerr = abs(self.targ_rwhl_vel) - abs(self.rwhl_vel)
+        rerr = np.abs(self.targ_rwhl_vel) - np.abs(self.rwhl_vel)
         rdcinc = self.K_P * rerr
         self.dutycycle_right += rdcinc
         if self.dutycycle_right >= 1.:
             self.dutycycle_right = 1.
         elif self.dutycycle_right <= 0.:
             self.dutycycle_right = 0
+        print(f"left/right error: {lerr, rerr}")
         print(f"left/right dutycycle: {self.dutycycle_left, self.dutycycle_right}")
         # drive motors
         if self.lwhl_dir > 0:
@@ -131,7 +134,7 @@ class HexaRobotCore(Node):
             self.left_motor.stop()
         if self.rwhl_dir > 0:
             self.right_motor.forward(self.dutycycle_right)
-        elif self.lwhl_dir < 0:
+        elif self.rwhl_dir < 0:
             self.right_motor.backward(self.dutycycle_right)
         else:
             self.right_motor.stop()